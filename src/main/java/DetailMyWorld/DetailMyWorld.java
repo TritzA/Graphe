8cf218ead75339da5125a9e9f2bfc051d67cc191
Zeynabdou <Zeynabdou@DESKTOP-G224M6K>
2020-11-19 02:40:22 -0500

    DetailMyWorld.Try1

--- src/main/java/DetailMyWorld/DetailMyWorld.java
+++ src/main/java/DetailMyWorld/DetailMyWorld.java
@@ -1,23 +1,100 @@
 package DetailMyWorld;
 
+import Maze.Tile;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class DetailMyWorld {
-    /** TODO
+    /**
+     * TODO
      * Returns a list of all continents with their respective countries within `world`
+     *
      * @param world 2D table of shape M x N representing the world
-     *                  0 : Water Region
-     *                  Positive non-null value : Country region for country of that specific value
+     * 0 : Water Region
+     * Positive non-null value : Country region for country of that specific value
      * @param isBreadthFirstSearch
-     *              If true, algorithm used should be Breadth-First Search
-     *              If false, algorithm used should be Depth-First Search
+     * If true, algorithm used should be Breadth-First Search
+     * If false, algorithm used should be Depth-First Search
      * @return List of all continents with their respective countries
-     *              Continents should be in order from left to right, top to bottom
-     *              Each continent should have its countries in order
+     * Continents should be in order from left to right, top to bottom
+     * Each continent should have its countries in order
      */
+
+
     public static List<Set<Integer>> findContinents(ArrayList<ArrayList<Integer>> world, boolean isBreadthFirstSearch) {
-        return null;
+        if (world.size() == 0)
+            return null;
+        int numberOfContinent = 0;
+        List<Set<Integer>> listOfAllContinents = new ArrayList<>(numberOfContinent);
+        if (isBreadthFirstSearch) {
+            for (int i = 0; i < world.size(); i++) {
+                for (int j = 0; j < world.get(i).size(); j++) {
+                    if (world.get(i).get(j) != 0) {
+                        numberOfContinent++;
+                        listOfAllContinents.add(dfs(world, i, j));
+                    }
+                }
+            }
+        }
+        else{
+            boolean con = true;
+        }
+        return listOfAllContinents;
+    }
+
+   private static Stack<Integer> setVoisins(int x, int y, ArrayList<ArrayList<Integer>> world) {
+       int l, r, u, d;
+       Stack<Integer> queue = new Stack<>();
+       queue.push(world.get(x).get(y));
+       try {
+           l = getValue(x, y - 1, world);
+            queue.push(l);
+       } catch (IndexOutOfBoundsException ignored) {
+       }
+
+       try {
+           r = getValue(x, y + 1, world);
+           queue.push(r);
+       } catch (IndexOutOfBoundsException ignored) {
+       }
+
+       try {
+           u = getValue(x - 1, y, world);
+           queue.push(u);
+       } catch (IndexOutOfBoundsException ignored) {
+       }
+
+       try {
+           d = getValue(x - 1, y, world);
+           queue.push(d);
+       } catch (IndexOutOfBoundsException ignored) {
+       }
+       return queue;
+   }
+
+    private static Integer getValue(int x, int y, ArrayList<ArrayList<Integer>> world){
+        return world.get(x).get(y);
+    }
+
+    public static Set<Integer> dfs(ArrayList<ArrayList<Integer>> world, int i, int j) {
+        Set<Integer> S = new HashSet<>();
+        S.add(world.get(i).get(j));
+        while (!S.isEmpty()) {
+            if (i < 0 || i > world.size() || j < 0 || j > world.get(i).size() || world.get(i).get(j) == 0) {
+                return null;
+            }
+            else
+                {
+                for(Integer edge : setVoisins(i, j, world))
+                {
+                  if (edge!=0){
+                      S.add(edge);
+                  }
+                }
+            }
+        }
+        return S;
     }
 
 
@@ -27,3 +104,6 @@ public class DetailMyWorld {
         }
     }
 }
+
+
+
--- src/main/java/Maze/Maze.java
+++ src/main/java/Maze/Maze.java
@@ -4,13 +4,89 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Maze {
+
     /** TODO
      * Returns the distance of the shortest path within the maze
      * @param maze 2D table representing the maze
      * @return Distance of the shortest path within the maze, null if not solvable
      */
+    private static void setVoisins(int x, int y, ArrayList<ArrayList<Tile>> maze) {
+        Tile l, r, u, d;
+        Tile th = maze.get(x).get(y);
+
+        try {
+            l = getValue(x, y - 1, maze);
+            th.addList(l);
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+        try {
+            r = getValue(x, y + 1, maze);
+            th.addList(r);
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+        try {
+            u = getValue(x - 1, y, maze);
+            th.addList(u);
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+        try {
+            d = getValue(x - 1, y, maze);
+            th.addList(d);
+        } catch (IndexOutOfBoundsException e) {
+        }
+    }
+
+    private static Tile getValue(int x, int y, ArrayList<ArrayList<Tile>> maze){
+        return maze.get(x).get(y);
+    }
+
     public static Integer findShortestPath(ArrayList<ArrayList<Tile>> maze) {
-        return null;
+        if (maze == null) {
+            throw new IllegalArgumentException("...");
+        }
+        Boolean con = false;
+        Stack<Tile> q = new Stack<Tile>();
+        for (int j = 0; j < maze.size(); j++) {
+            for(int i = 0; i < maze.get(j).size(); i++){
+                setVoisins(j, i, maze);
+                Tile th = maze.get(j).get(i);
+
+                if(th.toString().equals("*")){
+                    if(q.empty()) {
+                        th.setDist(0);
+                        q.push(th);
+                    }else
+                        con = true;
+                }else{
+                    th.setDist(-1);
+                }
+            }
+        }
+
+        if(!con){
+            return null;
+        }
+
+        while(!q.empty()){
+            System.out.println("AAAAAAAAAAAAAAAAAAAAA");
+            Tile t = q.pop();
+            System.out.println(t.getDist());
+            for(Tile p: t.getList()){
+                if(p.getDist() == -1) {
+                    System.out.println("BBBBBBBBBBB");
+                    p.setDist(t.getDist() + 1);
+                    q.push(p);
+                    if(p.toString().equals("*")){
+                        return p.getDist();
+                    }
+                }
+            }
+        }
+       return null;
+
     }
 
     public static void printMaze(ArrayList<ArrayList<Tile>> maze) {
