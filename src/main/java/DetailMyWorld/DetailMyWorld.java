6f764c77f980a4e753131340b33f2a2cf1765497
Zeynabdou <Zeynabdou@DESKTOP-G224M6K>
2020-11-20 18:51:48 -0500

    DetailMyWorld.Try4

--- src/main/java/DetailMyWorld/DetailMyWorld.java
+++ src/main/java/DetailMyWorld/DetailMyWorld.java
@@ -11,50 +11,98 @@ public class DetailMyWorld {
      * TODO
      * Returns a list of all continents with their respective countries within `world`
      *
-     * @param world 2D table of shape M x N representing the world
-     * 0 : Water Region
-     * Positive non-null value : Country region for country of that specific value
-     * @param isBreadthFirstSearch
-     * If true, algorithm used should be Breadth-First Search
-     * If false, algorithm used should be Depth-First Search
+     * @param world                2D table of shape M x N representing the world
+     *                             0 : Water Region
+     *                             Positive non-null value : Country region for country of that specific value
+     * @param isBreadthFirstSearch If true, algorithm used should be Breadth-First Search
+     *                             If false, algorithm used should be Depth-First Search
      * @return List of all continents with their respective countries
      * Continents should be in order from left to right, top to bottom
      * Each continent should have its countries in order
      */
 
-
     public static List<Set<Integer>> findContinents(ArrayList<ArrayList<Integer>> world, boolean isBreadthFirstSearch) {
-
-        List<Set<Integer>> listOfAllContinents = new ArrayList<>(); //La liste des continents à retourner
-        if(isBreadthFirstSearch){ // méthode avec breadthfirstsearch
-            for (int i = 0; i < world.size(); i++) {    // On parcours toute la matrice
-                for (int j = 0; j < world.get(i).size(); j++) {
-                    if (world.get(i).get(j) != 0) {  // On garde seulement les elements qui sont # 0
-                        listOfAllContinents.add(dfs(world, i, j)); // on fait un dfs en les mettant dans
-                    }                                              // dans la liste des continents
-                }                                               // le set elimine les doublons, d'ou son utilité
+        List<Set<Integer>> listOfContinent = new ArrayList<>();
+        for (int i = 0; i < world.size(); i++) {
+            for (int j = 0; j < world.get(i).size(); j++) {
+                if (world.get(i).get(j) != 0) {
+                    listOfContinent.add(dfs(world, i, j));
+                }
             }
-            }
-        return listOfAllContinents;
+        }
+        System.out.println(listOfContinent);
+        return listOfContinent;
     }
-// Voila le dfs, qui cherche tous les elements adjacents a un elements non nul,
-// ensuite l'adjacent de chaque element adjcent, ainside suite, on utilisé une méthode recursive ici
+
+    public static Set<Integer> dfs1(ArrayList<ArrayList<Integer>> world, int i, int j) {
+        Set<Integer> continent = new HashSet<>();
+        continent.add(world.get(i).get(j));
+        world.get(i).set(j, 0);
+        if (i != 0) {
+            if (world.get(i - 1).get(j) != 0)
+                continent.addAll(Objects.requireNonNull(dfs(world, i - 1, j)));
+        }
+
+        if (i != world.size() - 1) {
+            if (world.get(i + 1).get(j) != 0)
+                continent.addAll(Objects.requireNonNull(dfs(world, i + 1, j)));
+        }
+        if (j != 0) {
+            if (world.get(i).get(j - 1) != 0)
+                continent.addAll(Objects.requireNonNull(dfs(world, i, j - 1)));
+        }
+        if (j != world.get(i).size() - 1) {
+            if (world.get(i).get(j + 1) != 0)
+                continent.addAll(Objects.requireNonNull(dfs(world, i, j + 1)));
+        }
+        return continent;
+    }
 
     public static Set<Integer> dfs(ArrayList<ArrayList<Integer>> world, int i, int j) {
-        Set<Integer> S = new HashSet<>(); // on utilise ici un set, car on retourne une liste de set
-        while (!S.isEmpty()) {            // qui est listeOfAllContinents
-                try {
-                    S.add(world.get(i).get(j)); // On met l'élément dans le set
-                    world.get(i).set(j, 0);     // et on le change à zéro pour éviter de le reprendre
-                    dfs(world, i - 1, j); //up
-                    dfs(world, i + 1, j); //down
-                    dfs(world, i, j + 1); // right
-                    dfs(world, i, j - 1); //left
-                } catch (IndexOutOfBoundsException ignored) {
+        int l, r, u, d;
+        Set<Integer> continent = new HashSet<>();
+        Stack<Integer> row = new Stack<>();
+        Stack<Integer> col = new Stack<>();
+        row.push(i);
+        col.push(j);
+        while (!row.isEmpty()) {
+            int x, y;
+            x = col.pop();
+            y = row.pop();
+            continent.add(world.get(y).get(x));
+            for (int k = 0; k < 4; k++) {
+                switch (k) {
+                    case 0:
+                        if (x != 0) {
+                            col.push(x - 1);
+                            row.push(y);
+                        }
+                        break;
+                    case 1:
+                        if (x != world.get(y).size() - 1) {
+                            col.push(x + 1);
+                            row.push(y);
+                        }
+                        break;
+                    case 2:
+                        if (y != 0) {
+                            col.push(x);
+                            row.push(y - 1);
+                        }
+                        break;
+                    case 3:
+                        if (y != world.size()-1) {
+                            col.push(x);
+                            row.push(y + 1);
+                        }
+                        break;
+                }
             }
+
         }
-        return S;    // on retourne un continent, qui est un set
-        }
+
+        return continent;
+}
 
     public static void printWorld(ArrayList<ArrayList<Integer>> world) {
         for (ArrayList<Integer> row : world) {
@@ -62,6 +110,7 @@ public class DetailMyWorld {
         }
     }
 }
+
 
 
 
