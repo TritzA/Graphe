12600808ab6cc74fb123b451155b23489cd28a76
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2020-11-20 21:43:15 -0500

    plus propre

--- src/main/java/DetailMyWorld/DetailMyWorld.java
+++ src/main/java/DetailMyWorld/DetailMyWorld.java
@@ -1,10 +1,7 @@
 package DetailMyWorld;
 
-import Maze.Tile;
-
 import java.util.*;
 import java.util.stream.Collectors;
-import java.lang.IndexOutOfBoundsException;
 
 public class DetailMyWorld {
     /**
@@ -20,64 +17,129 @@ public class DetailMyWorld {
      * Continents should be in order from left to right, top to bottom
      * Each continent should have its countries in order
      */
-    //private static boolean[][] isVisited = new boolean [100000][100000];
+
     public static List<Set<Integer>> findContinents(ArrayList<ArrayList<Integer>> world, boolean isBreadthFirstSearch) {
         List<Set<Integer>> listOfContinent = new ArrayList<>();
         for (int i = 0; i < world.size(); i++) {
             for (int j = 0; j < world.get(i).size(); j++) {
                 if (world.get(i).get(j) != 0) {
-                    listOfContinent.add(dfs(world, i, j));
+                    if (isBreadthFirstSearch) {
+                        listOfContinent.add(bfs(world, i, j));
+                    } else {
+                        listOfContinent.add(dfs(world, i, j));
+                    }
                 }
             }
         }
-        System.out.println(listOfContinent);
         return listOfContinent;
     }
 
-    public static Set<Integer> dfs(ArrayList<ArrayList<Integer>> world, int i, int j) {
+    public static Set<Integer> bfs(ArrayList<ArrayList<Integer>> world, int i, int j) {
         Set<Integer> continent = new HashSet<>();
-        if (world.get(i).get(j) == 0) {
-            return continent;
-        }
-        continent.add(world.get(i).get(j));
+        Queue<Integer> row = new ArrayDeque<>();
+        Queue<Integer> col = new ArrayDeque<>();
 
-        world.get(i).set(j, 0);
+        row.offer(i);
+        col.offer(j);
 
-        if (i != 0) {
-            continent.addAll(Objects.requireNonNull(dfs(world, i - 1, j)));
-        }
+        while (!row.isEmpty()) {
 
-        if (i != world.size() - 1) {
-            continent.addAll(Objects.requireNonNull(dfs(world, i + 1, j)));
-        }
-        if (j != 0) {
-            continent.addAll(Objects.requireNonNull(dfs(world, i, j - 1)));
-        }
-        if (j != world.get(i).size() - 1) {
-            continent.addAll(Objects.requireNonNull(dfs(world, i, j + 1)));
+            int x, y;
+            x = col.poll();
+            y = row.poll();
+            if (world.get(y).get(x) != 0) {
+                continent.add(world.get(y).get(x)); // optimisation ?
+            }
+
+            world.get(y).set(x, 0);
+            if (x != 0) {
+                if (world.get(y).get(x - 1) != 0) {
+                    col.offer(x - 1);
+                    row.offer(y);
+                    continent.add(world.get(y).get(x - 1));
+                    world.get(y).set(x - 1, 0);
+                }
+            }
+
+            if (x != world.get(y).size() - 1) {
+                if (world.get(y).get(x + 1) != 0) {
+                    col.offer(x + 1);
+                    row.offer(y);
+                    continent.add(world.get(y).get(x + 1));
+                    world.get(y).set(x + 1, 0);
+                }
+            }
+
+            if (y != 0) {
+                if (world.get(y - 1).get(x) != 0) {
+                    col.offer(x);
+                    row.offer(y - 1);
+                    continent.add(world.get(y - 1).get(x));
+                    world.get(y - 1).set(x, 0);
+                }
+            }
+
+            if (y != world.size() - 1) {
+                if (world.get(y + 1).get(x) != 0) {
+                    col.offer(x);
+                    row.offer(y + 1);
+                    continent.add(world.get(y + 1).get(x));
+                    world.get(y + 1).set(x, 0);
+                }
+            }
+
         }
         return continent;
     }
 
-    public static Set<Integer> bfs(ArrayList<ArrayList<Integer>> world, int i, int j) {
-        int l, r, u, d;
-        for (int k = 0; k < 4; k++) {
-            switch (i) {
-                case 0:
-                    l = world.get(i).get(j - 1);
-                    break;
-                case 1:
-                    r = world.get(i).get(j + 1);
-                    break;
-                case 2:
-                    u = world.get(i - 1).get(j);
-                    break;
-                case 3:
-                    l = world.get(i + 1).get(j);
-                    break;
+    public static Set<Integer> dfs(ArrayList<ArrayList<Integer>> world, int i, int j) {
+        Set<Integer> continent = new HashSet<>();
+        Stack<Integer> row = new Stack<>();
+        Stack<Integer> col = new Stack<>();
+
+        row.push(i);
+        col.push(j);
+
+        while (!row.isEmpty()) {
+
+            int x, y;
+            x = col.pop();
+            y = row.pop();
+            if (world.get(y).get(x) != 0) {
+                continent.add(world.get(y).get(x));
+                world.get(y).set(x, 0);
             }
+
+            if (x != 0) {
+                if (world.get(y).get(x - 1) != 0) {
+                    col.push(x - 1);
+                    row.push(y);
+                }
+            }
+
+            if (x != world.get(y).size() - 1) {
+                if (world.get(y).get(x + 1) != 0) {
+                    col.push(x + 1);
+                    row.push(y);
+                }
+            }
+
+            if (y != 0) {
+                if (world.get(y - 1).get(x) != 0) {
+                    col.push(x);
+                    row.push(y - 1);
+                }
+            }
+
+            if (y != world.size() - 1) {
+                if (world.get(y + 1).get(x) != 0) {
+                    col.push(x);
+                    row.push(y + 1);
+                }
+            }
+
         }
-        return null;
+        return continent;
     }
 
     public static void printWorld(ArrayList<ArrayList<Integer>> world) {
@@ -86,7 +148,3 @@ public class DetailMyWorld {
         }
     }
 }
-
-
-
-
