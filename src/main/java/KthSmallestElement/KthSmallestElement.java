60a01c0ec2bb75a9b943d2d3e993949c494e7ec3
Antonin Tritz <45083857+TritzA@users.noreply.github.com>
2020-11-27 19:13:36 -0500

    final

--- src/main/java/KthSmallestElement/KthSmallestElement.java
+++ src/main/java/KthSmallestElement/KthSmallestElement.java
@@ -1,7 +1,6 @@
 package KthSmallestElement;
 
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class KthSmallestElement {
     /**
@@ -25,16 +24,34 @@ public class KthSmallestElement {
      * @param k      Index of the smallest element we want
      * @return `K`th smallest element in `matrix`, null if non-existent
      */
+
+    /**
+     * Explication des complexités de la résolution du problème
+     * <p>
+     * Explication de la complexité temporelle
+     * On devra itérer sur k éléments. Et pour chaque élément itéré, on devra
+     * l'ajouter dans un Heap (grâce au add) et le sortir d'un Heap (grâce au poll).
+     * Ces deux opérations s'effectuent en O( log ( max(m,n) ) ). La complexité
+     * sera donc de O( k*log( max(m, n) ) ).
+     * <p>
+     * Explication de la complexité spatiale
+     * Au début on ajoute tous les éléments de la première ligne dans le Heap.
+     * Ensuite, à chaque fois qu'on sort un élément du heap, on le compare,
+     * puis on en ajoute un autre. Ce qui fait que la taille du Heap reste constante
+     * à une valeur près. Elle reste d'une taille O ( max(m,n) ).
+     * <p>
+     * Note, les complexités sont explicitées dans le code en commentaires.
+     */
     static public <T extends Comparable<T>> T findKthSmallestElement(final T[][] matrix, final int k) {
 
         class MatrixElement implements Comparable<MatrixElement> {
-            final int rIndex;
-            final int cIndex;
+            final int rowIndex;
+            final int colIndex;
             final T value;
 
             public MatrixElement(T val, int rIdx, int cIdx) {
-                this.rIndex = rIdx;
-                this.cIndex = cIdx;
+                this.rowIndex = rIdx;
+                this.colIndex = cIdx;
                 this.value = val;
             }
 
@@ -44,8 +61,8 @@ public class KthSmallestElement {
             }
         }
 
-        Queue<MatrixElement> queue = new PriorityQueue<>();
-        T returnValue;
+        PriorityQueue<MatrixElement> queue = new PriorityQueue<>();// Max size : max(m, n) -> O ( max(m,n) ) Space complexity
+
         if (matrix != null) {
             for (int j = 0; j < matrix[0].length; j++) {
                 queue.add(new MatrixElement(matrix[0][j], 0, j));
@@ -53,15 +70,16 @@ public class KthSmallestElement {
         } else {
             return null;
         }
+
         int count = 0;
-        while (!queue.isEmpty()) {
-            MatrixElement mat = queue.poll();
+        while (!queue.isEmpty()) {// O(k) * ( O( log( max(m, n) ) ) + O( log( max(m, n) ) ) ) -> O( k * log( max(m, n) ) ) Time complexity
+            MatrixElement mat = queue.poll();// O( log( max(m, n) ) ) Time complexity
             if (count == k) {
                 return mat.value;
             } else {
                 count++;
-                if (mat.rIndex + 1 < matrix.length) {
-                    queue.add(new MatrixElement(matrix[mat.rIndex + 1][mat.cIndex], mat.rIndex + 1, mat.cIndex));
+                if (mat.rowIndex + 1 < matrix.length) {
+                    queue.add(new MatrixElement(matrix[mat.rowIndex + 1][mat.colIndex], mat.rowIndex + 1, mat.colIndex));// O( log( max(m, n) ) ) Time complexity
                 }
             }
         }
